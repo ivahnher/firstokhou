b0add60bbdf2e7540fb530c5926c7a45e7b10162
ivahnher <elcuquerias@gmail.com>
2022-05-05 17:52:55 +0000

    first contract

--- smartcon/fiertstok.sol
+++ smartcon/fiertstok.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 // crear contrato personal y ( funcion (constructor) se corre al principio y por unica vez tu pudes tener monedas)
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.3/contracts/token/ERC20/ERC20.sol";
 
-contract FIRSTOKHOU is ERC20 {
+contract fiertstok is ERC20 {
     constructor () public ERC20("firstokhou", "fth1") {
     //primera y unica vez.
     // mint 100 tokens a wallet conmtrato.
