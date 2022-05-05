048b39c718b70716f00adb716715a6631c93316a
ivahnher <elcuquerias@gmail.com>
2022-05-05 17:47:49 +0000

    ready for erc20

--- smartcon/fiertstok.sol
+++ smartcon/fiertstok.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 // importar otros contratos.
 
 // crear contrato personal y ( funcion (constructor) se corre al principio y por unica vez tu pudes tener monedas)
-//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.3/contracts/token/ERC20/ERC20.sol";
 
 contract FIRSTOKHOU is ERC20 {
